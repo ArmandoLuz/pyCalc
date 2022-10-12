d644f72a121c705205f192bae5ef4ef08a790eb8
Luís Clício <cliciosa14@gmail.com>
2022-10-12 17:36:46 -0300

    feat: implements history screen

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-import kivy 
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-# from kivy.uix.widget import Widget
-from kivy.uix.gridlayout import GridLayout
-# from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.scrollview import ScrollView
 
 from lib.calculator import Calculator
 
@@ -20,14 +13,6 @@ class ScreenManagement(ScreenManager):
 
 
 class CalculatorScreen(Screen):
-    pass
-
-
-class HistoryScreen(Screen):
-    pass
-
-
-class CalculatorUI(GridLayout):
     def calculate(self, expression):
         if not expression:
             return
@@ -36,37 +21,26 @@ class CalculatorUI(GridLayout):
         self.display.text = str(result) if result else 'Operação inválida!'
 
 
-class HistoryUI(GridLayout):
+class HistoryScreen(Screen):
+    def on_enter(self):
+        self.load_history()
+
     def load_history(self):
+        self.list_view.text = self.get_history()
+
+    def get_history(self):
         history = calculator.history.items()
-        # result = '\n'.join(list(map(lambda item: f'{item[0]} = {item[1]}', history)))
         result = [f'{item[0]} = {item[1]}' for item in history]
-        print(calculator.history.items())
-        return 'kdskdks'
-    # def __init__(self, **kwargs):
-    #     super().__init__(**kwargs)
-
-    #     # self.cols = 1
-    #     # self.rows = 2
-
-    #     self._scrollview = ScrollView()
-
-    #     for expression, value in calculator.history.items():
-    #         print(f'{expression} = {value}')
-    #         self._scrollview.add_widget(Button(text=f'{expression} = {value}'))
-
-    #     self.add_widget(self._scrollview)
-
-        # self._clear_button = Button(text='Limpar Histórico')
+        return '\n'.join(result)
 
     def clear(self):
         calculator.clear_history()
+        self.load_history()
 
 
 class PyCalc(App):
     def build(self):
         return ScreenManagement()
-        # return CalculatorUI()
 
     
 if __name__ == '__main__':
